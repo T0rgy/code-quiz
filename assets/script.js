1938be5b0fddd6eab0394448c6b6b725e345a82c
Torgy <jctorge@gmail.com>
2022-03-27 12:18:33 -0500

    function start

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//Global veriables
+//Global variables
 var time = 60;
 var timer = document.getElementById("timer");
 var timeLeft = document.getElementById("timeLeft");
@@ -6,7 +6,7 @@ var timesUp = document.getElementById("timesUp");
 var homePage = document.getElementById("homePage");
 var startQBtn = document.getElementById("startQ");
 var questionPlace = document.getElementById("question");
-var questionDiv = document.getElementById("quiz-area")
+var quizArea = document.getElementById("quiz-area")
 var choice1 = document.getElementById("c1");
 var choice2 = document.getElementById("c2");
 var choice3 = document.getElementById("c3");
@@ -56,4 +56,61 @@ var questions = [
 
 
 //functions
+
+function quiz() {
+  questionIndex = 0;
+  time = 59;
+  timeLeft.textContent = time;
+  initialInput.textContent = "";
+
+  homePage.style.display = "none";
+  quizArea.style.display = "block";
+  timer.style.display = "block";
+  timesUp.style.display = "none";
+
+  var startTimer = setInterval(function() {
+    time--;
+    timeLeft.textContent = time;
+    if (time <= 0) {
+      clearInterval(startTimer);
+      if (questionIndex < question.length - 1) {
+        gameOver();
+      }
+    }
+  }, 1000);
+  showQuiz();
+};
+
+function showQuiz() {
+  nextQuestion();
+};
+
+function nextQuestion() {
+  questionPlace.textContent = questions[questionIndex].question;
+  choice1.textContent = questions[questionIndex].choices[0];
+  choice2.textContent = questions[questionIndex].choices[1];
+  choice3.textContent = questions[questionIndex].choices[2];
+  choice4.textContent = questions[questionIndex].choices[3];
+};
+
+function answerResult(answer) {
+  var line = document.getElementById("line")
+  line.style.display = "block";
+  answerResult.style.display = "block";
+  if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+    correctAnswer++;
+    answerResult.textContent = "Correct!";
+  } else {
+    time -= 10;
+    timeLeft.textContent = time;
+    answerResult.textContent = "Wrong... The correct answer was " + questions[questionIndex].answer;
+  }
+
+  questionIndex++;
+  if (questionIndex < questions.length) {
+    nextQuestion;
+  } else {
+    gameOver();
+  }
+};
 
