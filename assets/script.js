fa40c1f34dde9ec269a487a7c6491a3a3468aaf4
Torgy <jctorge@gmail.com>
2022-03-27 09:53:15 -0500

    added questions array

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,41 @@
+//Global veriables
 var timer = startTimer();
 var time = 60;
-var question = document.getElementById("question");
+var questionPlace = document.getElementById("question");
 var choice1 = document.getElementById("c1");
 var choice2 = document.getElementById("c2");
 var choice3 = document.getElementById("c3");
 var choice4 = document.getElementById("c4");
 var right = time + 10;
 var wrong = time - 10;
+
+//Questions array
+var questions = [
+  {
+    question: "To see if two variables are equal in an if / else statement you would use ____.",
+    choices: ["1. =+", "2. ==", "3. 'equals'", "4. !="],
+    answer: "2. =="
+},
+{
+  question: "How to write an IF statement in JavaScript?",
+  choices: ["1. if i == 5 then", "2. if i = 5 then", "3. if(i == 5)", "4. if i = 5"],
+  answer: "3. if(i == 5)"
+},
+{
+    question: "The first index of an array is ____.",
+    choices: ["1. 0", "2. 1", "3. 4", "4. any"],
+    answer: "1. 0"
+},
+{
+    question: "Who invented JavaScript?",
+    choices: ["1. Beyonce", "2. Joe Biden", "3. Brendan Eich", "4. Ben Javascript"],
+    answer: "3. Brendan Eich"
+},
+{
+    question: "How do you add a comment in a JavaScript?",
+    choices: ["1. //This is a comment", "2. <!--This is a comment-->", "3. 'This is a comment", "4. * This is a comment *"],
+    answer: "1. //This is a comment"
+}];
 
 
 
@@ -30,5 +59,4 @@ function startTimer(){
 function start(){
     document.getElementById("startQ");
     startTimer();
-    document.getElementById("homePage")
 };
